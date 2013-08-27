7ceb46c03d3d3c74f3ed0635c6cf7a000b27f310
Piyush <piykumar@gmail.com>
2013-08-27 05:45:03 -0400

    extract cookie from http header cURL

--- random-stuff/commands.sh
+++ random-stuff/commands.sh
@@ -3,6 +3,9 @@ tcpdump -ni eth0 -s0 host 4.2.2.2 -w tcpdump.pcap
 
 #JSESSIONID retrieve from cookie logs
 awk 'match($0, /JSESSIONID=([^;]*);/, a) {print a[1]}'
+
+# cURL extract the cookie uuid
+curl -v http://example.com 2>&1 | awk 'match($0, /UUID=([^;]*);/, a) {print a[1]}'
  
 # Simple install of dev packages 
 yum groupinstall 'Development Tools'
